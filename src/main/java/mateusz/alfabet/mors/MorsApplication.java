70403d9354ed6e0cb04a7aebb589156465e24cb2
Pawel Michalski <pawel.michalski@ipfdigital.com>
2020-05-08 16:03:22 +0200

    poczatek

---
+++ src/main/java/mateusz/alfabet/mors/MorsApplication.java
@@ -0,0 +1,13 @@
+package mateusz.alfabet.mors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mateusz/alfabet/mors/MorsApplicationTests.java
@@ -0,0 +1,13 @@
+package mateusz.alfabet.mors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
